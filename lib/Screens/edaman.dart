fab8a5c036041deefa4129af911816f629e77f95
sugarete <cthien3602@gmail.com>
2023-11-28 00:10:23 +0700

    Added Edamam API search page

--- lib/Screens/edaman.dart
+++ lib/Screens/edaman.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:myrecipes/model/edamam_model.dart';
+import 'package:myrecipes/Screens/detail.dart';
 
 class EdamanPage extends StatefulWidget {
   const EdamanPage({super.key});
@@ -8,17 +10,137 @@ class EdamanPage extends StatefulWidget {
 }
 
 class _EdamanPageState extends State<EdamanPage> {
+  final TextEditingController _searchController = TextEditingController();
+  final EdamamApiService _apiService =
+      EdamamApiService('bb937e3229ccafaaba075384dd76260e');
+  List<EdamamRecipeModel> _searchResults = [];
+
+  void _searchRecipes() async {
+    final query = _searchController.text;
+    if (query.isNotEmpty) {
+      final results = await _apiService.searchRecipes(query);
+      setState(() {
+        _searchResults =
+            results.map((recipe) => EdamamRecipeModel.fromMap(recipe)).toList();
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
         child: Column(
           children: [
-            TextField(),
+            TextField(
+              controller: _searchController,
+              decoration: const InputDecoration(labelText: 'Enter a recipe'),
+              onSubmitted: (_) => _searchRecipes(),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _searchRecipes,
+              child: const Text('Search'),
+            ),
+            const SizedBox(height: 20),
+            Expanded(
+              child: _searchResults.isNotEmpty
+                  ? ListView.builder(
+                      physics: const ScrollPhysics(),
+                      shrinkWrap: true,
+                      itemCount: _searchResults.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return Padding(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 12,
+                            vertical: 12,
+                          ),
+                          child: GestureDetector(
+                            onTap: () => Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => EdamamRecipeDetail(
+                                  recipeModel: _searchResults[index],
+                                ),
+                              ),
+                            ),
+                            child: EdamamRecipeCard(
+                              _searchResults[index],
+                            ),
+                          ),
+                        );
+                      },
+                    )
+                  : const Center(
+                      child: Text('No recipes found.'),
+                    ),
+            ),
           ],
         ),
       ),
+    );
+  }
+}
+
+class EdamamRecipeCard extends StatefulWidget {
+  final EdamamRecipeModel recipeModel;
+  const EdamamRecipeCard(this.recipeModel, {super.key});
+
+  @override
+  State<EdamamRecipeCard> createState() => _EdamamRecipeCardState();
+}
+
+class _EdamamRecipeCardState extends State<EdamamRecipeCard> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Stack(
+          children: [
+            Align(
+              alignment: Alignment.topCenter,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(24),
+                child: Hero(
+                  tag: widget.recipeModel.image,
+                  child: Image(
+                    height: 350,
+                    width: 350,
+                    fit: BoxFit.cover,
+                    image: NetworkImage(widget.recipeModel.image),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 33,
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Flexible(
+                flex: 2,
+                child: Column(
+                  children: [
+                    Text(
+                      widget.recipeModel.title.toUpperCase(),
+                      style: Theme.of(context).textTheme.titleMedium,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
--- lib/model/edamam_model.dart
+++ lib/model/edamam_model.dart
@@ -1,15 +1,45 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
-class EdamanPage extends StatefulWidget {
-  const EdamanPage({super.key});
+class EdamamApiService {
+  final String apiKey;
+  final String apiUrl = 'https://api.edamam.com/api/recipes/v2';
 
-  @override
-  State<EdamanPage> createState() => _EdamanPageState();
+  EdamamApiService(this.apiKey);
+
+  Future<List<dynamic>> searchRecipes(String query) async {
+    final response = await http.get(
+      Uri.parse('$apiUrl?type=public&q=$query&app_id=b0eefc7c&app_key=$apiKey'),
+    );
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> data = json.decode(response.body);
+      return data['hits'];
+    } else {
+      throw Exception('Failed to load recipes');
+    }
+  }
 }
 
-class _EdamanPageState extends State<EdamanPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
+class EdamamRecipeModel {
+  String title;
+  String source;
+  String image;
+  List<String> ingredients;
+
+  EdamamRecipeModel({
+    required this.title,
+    required this.source,
+    required this.image,
+    required this.ingredients,
+  });
+
+  // Method to convert a Map to a RecipeModel
+  factory EdamamRecipeModel.fromMap(Map<String, dynamic> map) {
+    return EdamamRecipeModel(
+      title: map['recipe']['label'],
+      source: map['recipe']['source'],
+      image: map['recipe']['image'],
+      ingredients: List<String>.from(map['recipe']['ingredients'].map((ingredient) => ingredient['text'])),
+    );
   }
 }
